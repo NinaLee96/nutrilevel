d9c3c02ee7292c937ba5234d0e76aec67ae176f3
Steven Ryan Ramos <43050949+rsteven46@users.noreply.github.com>
2019-02-23 17:55:01 -0800

    login page  skelton

--- App.js
+++ App.js
@@ -1,62 +1,67 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity, StatusBar,Platform,TextInput, Alert, Button } from 'react-native';
+import Expo, { Constants, Location, Permissions,AppLoading, Asset, Font, Icon } from 'expo';
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
 
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      username:'',
+      password:'',
+
+    };
+  }
+  onLogin() {
+    const { username, password } = this.state;
+
+    Alert.alert('Credentials', `${username} + ${password}`);
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
+  render() {
+    return ( <View style={styles.container}>
+      <TextInput
+        value={this.state.username}
+        onChangeText={(username) => this.setState({ username })}
+        placeholder={'Username'}
+        style={styles.input}
+      />
+      <TextInput
+        value={this.state.password}
+        onChangeText={(password) => this.setState({ password })}
+        placeholder={'Password'}
+        secureTextEntry={true}
+        style={styles.input}
+      />
+      
+      <Button
+        title={'Login'}
+        style={styles.input}
+        onPress={this.onLogin.bind(this)}
+      />
+    </View>
 
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
   },
 });
